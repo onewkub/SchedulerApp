92402d728cbd99411e54020edb596ae535d76ce5
Nattakit Hosapsin <relta@chifumi.net>
2019-10-27 20:13:13 +0700

    Improve routing (#11)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,22 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
-import {SideNavBarComponent} from './components/side-nav-bar/side-nav-bar.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {AuthGuard} from './guards/auth.guard';
 import {AddProjectComponent} from './components/add-project/add-project.component';
 import {ProjectDetailComponent} from './components/project-detail/project-detail.component';
+import {BaseComponent} from './components/base/base.component';
 
 const routes: Routes = [
   {
-    path: 'dashboard', component: SideNavBarComponent, children: [
-      {path: '', component: DashboardComponent},
-      {path: '', pathMatch: 'full', redirectTo: ''},
+    path: 'app', component: BaseComponent, children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'projects/:pid', component: ProjectDetailComponent},
       {path: 'addProject', component: AddProjectComponent},
-    ], canActivate: [AuthGuard]
-  },
-  {
-    path: 'projects/:pid', component: SideNavBarComponent,
-    children: [{path: '', component: ProjectDetailComponent}]
+      {path: '', pathMatch: 'full', redirectTo: 'dashboard'}
+    ]
   },
   {path: '', component: HomeComponent},
-  {path: '', pathMatch: 'full', redirectTo: ''}
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
